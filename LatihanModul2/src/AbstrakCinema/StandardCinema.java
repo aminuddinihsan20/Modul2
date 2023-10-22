9c6aa886dc0b1ab3a308439dd75e23159df7b16e
Aminz <3411211078.aminuddin.ihsan@gmail.com>
2023-10-22 18:39:00 +0700

    Menambahkan folder dan isi file

---
+++ LatihanModul2/src/AbstrakCinema/StandardCinema.java
@@ -0,0 +1,12 @@
+package AbstrakCinema;
+
+class StandardCinema extends Cinema {
+    public StandardCinema(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    Double calculateAdminFee() {
+        return price * 10 / 100;
+    }
+}
